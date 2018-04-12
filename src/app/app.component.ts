210dc94e1c7db12c06d11edcdfec03c29d063859
stevenvjsly <stevenvjsly@gmail.com>
2018-04-11 20:28:56 -0500

    imported cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Card } from '../models/card';
+import { CARDS } from '../models/cards';
 
 @Component({
   selector: 'app-root',
@@ -8,60 +9,7 @@ import { Card } from '../models/card';
 })
 export class AppComponent {
   // title = 'app';
-  cards = [
-    new Card('A', 1, 'Hearts'),
-    new Card('2', 2, 'Hearts'),
-    new Card('3', 3, 'Hearts'),
-    new Card('4', 4, 'Hearts'),
-    new Card('5', 5, 'Hearts'),
-    new Card('6', 6, 'Hearts'),
-    new Card('7', 7, 'Hearts'),
-    new Card('8', 8, 'Hearts'),
-    new Card('9', 9, 'Hearts'),
-    new Card('10', 10, 'Hearts'),
-    new Card('J', 10, 'Hearts'),
-    new Card('Q', 10, 'Hearts'),
-    new Card('K', 10, 'Hearts'),
-    new Card('A', 1, 'Clubs'),
-    new Card('2', 2, 'Clubs'),
-    new Card('3', 3, 'Clubs'),
-    new Card('4', 4, 'Clubs'),
-    new Card('5', 5, 'Clubs'),
-    new Card('6', 6, 'Clubs'),
-    new Card('7', 7, 'Clubs'),
-    new Card('8', 8, 'Clubs'),
-    new Card('9', 9, 'Clubs'),
-    new Card('10', 10, 'Clubs'),
-    new Card('J', 10, 'Clubs'),
-    new Card('Q', 10, 'Clubs'),
-    new Card('K', 10, 'Clubs'),
-    new Card('A', 1, 'Spades'),
-    new Card('2', 2, 'Spades'),
-    new Card('3', 3, 'Spades'),
-    new Card('4', 4, 'Spades'),
-    new Card('5', 5, 'Spades'),
-    new Card('6', 6, 'Spades'),
-    new Card('7', 7, 'Spades'),
-    new Card('8', 8, 'Spades'),
-    new Card('9', 9, 'Spades'),
-    new Card('10', 10, 'Spades'),
-    new Card('J', 10, 'Spades'),
-    new Card('Q', 10, 'Spades'),
-    new Card('K', 10, 'Spades'),
-    new Card('A', 1, 'Diamonds'),
-    new Card('2', 2, 'Diamonds'),
-    new Card('3', 3, 'Diamonds'),
-    new Card('4', 4, 'Diamonds'),
-    new Card('5', 5, 'Diamonds'),
-    new Card('6', 6, 'Diamonds'),
-    new Card('7', 7, 'Diamonds'),
-    new Card('8', 8, 'Diamonds'),
-    new Card('9', 9, 'Diamonds'),
-    new Card('10', 10, 'Diamonds'),
-    new Card('J', 10, 'Diamonds'),
-    new Card('Q', 10, 'Diamonds'),
-    new Card('K', 10, 'Diamonds')
-  ];
+  cards = CARDS;
 
   // General Playing Card Vars and Logic
   DECKSIZE = 52;
